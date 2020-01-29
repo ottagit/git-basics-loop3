e4a5c083118978057c2ee156da5105573edb80e9
ottagit <ottacc66@gmail.com>
2020-01-29 12:14:27 +0300

    examine data tyoes in JS

---
+++ data_types.js
@@ -0,0 +1,23 @@
+// Scope bindings: var, let, const
+console.log('Understanding var')
+var name; // variable declaration
+name = 'Chris Otta'; // variable assignment of type 'String'
+console.log(name);
+var name = 'Fleria Kate';
+console.log(name);
+
+console.log('Understanding let')
+let fruit = 'Mango';
+console.log(fruit);
+fruit = 'Pineapple';
+console.log(fruit);
+// Expect an exception to be thrown
+//let fruit = 'Pawpaw';
+//console.log(fruit);
+
+// Const in action
+const PI = 3.142;
+console.log(PI);
+// Error or warning upon reassignment of a constant
+PI = 12.32;
+console.log(PI);
